62e16eaf8709c87a2dce04a00e3a03ab7271a165
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-12 06:46:23 +0100

    removed hinting on class

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """Where can I learn Python?"""
-from pymongo.collection import Collection
-"""Collection is class"""
-from typing import Type
 
 
-def schools_by_topic(mongo_collection: Type[Collection], topic: str):
+def schools_by_topic(mongo_collection, topic: str):
     """Write a Python function that returns the list of school having a specific topic:
 
         Prototype: def schools_by_topic(mongo_collection, topic):

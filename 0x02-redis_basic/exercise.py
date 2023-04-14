7017a0f276e38789371a3cbefc648ddede430f60
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-15 00:13:07 +0100

    update to exercise and web

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,115 +1,104 @@
 #!/usr/bin/env python3
-"""Cache module"""
-from redis import Redis
-from uuid import uuid4
-from typing import Any, Callable, Union
+'''A module for using the Redis NoSQL data storage.
+'''
 from functools import wraps
+from typing import Any, Callable, Union
+from redis import Redis
+import uuid
 
 
-def count_calls(f: Callable) -> Callable:
-    """Calculates how many times the function is called"""
-    @wraps(f)
+def count_calls(method: Callable) -> Callable:
+    '''Tracks the number of calls made to a method in a Cache class.
+    '''
+    @wraps(method)
     def wrapper(self, *args, **kwargs) -> Any:
-        """
-        wrapper preserves the functions documentation and
-        function name
-        """
-        if isinstance(self._redis, Redis):
-            self._redis.incr(f.__qualname__)
-        return f(self, *args, **kwargs)
+        '''returns the given method after incrementing its call counter.
+        '''
+
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
     return wrapper
 
 
-def call_history(f: Callable) -> Callable:
-    """
-    Everytime the original function will be called, we will
-    add its input parameters to one list in redis, and store
-    its output into another list.
-    """
-    @wraps(f)
-    def wrapper(self, *args, **kwargs) -> Any:
-        """
-        wrapper preserves the functions documentation and
-        function name
-        """
-        f_ret = f(self, *args, **kwargs)
-        if isinstance(self._redis, Redis):
-            self._redis.rpush(
-                '{}:inputs'.format(f.__qualname__),
-                str(args)
-            )
-            self._redis.rpush(
-                '{}:outputs'.format(f.__qualname__),
-                f_ret
-            )
-        return f_ret
-    return wrapper
+def call_history(method: Callable) -> Callable:
+    '''Tracks the call details of a method in a Cache class.
+    '''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''Returns the method's output after storing its inputs and output.
+        '''
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
 
 
-def replay(f: Callable) -> None:
-    """
-    In this tasks, we will implement a replay function to display the
-    history of calls of a particular function.
-
-    Use keys generated in previous tasks to generate the following output:
-    """
-    if f is None or not hasattr(f.__self__, '_redis'):
+def replay(fn: Callable) -> None:
+    '''Displays the call history of a Cache class' method.
+    '''
+    if fn is None or not hasattr(fn, '__self__'):
         return
-    store = getattr(f.__self__, '_redis', None)
-    if not isinstance(store, Redis):
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
         return
-    func_name = f.__qualname__
-    inputs = '{}:inputs'.format(func_name)
-    outputs = '{}:outputs'.format(func_name)
-
-    print('{} was called {} times:'.format(
-        func_name,
-        0 if store.exists(func_name) == 0 and
-        not store.get(func_name) else store.get(func_name)
-    ))
-    func_inputs = store.lrange(inputs, 0, -1)
-    func_outputs = store.lrange(outputs, 0, -1)
-    for input, output in zip(func_inputs, func_outputs):
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
         print('{}(*{}) -> {}'.format(
-            func_name,
-            input.decode('utf-8'),
-            output
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
         ))
 
 
 class Cache:
-    """
-    Class implements the redis caching
-    """
+    '''Represents an object for storing data in a Redis data storage.
+    '''
 
     def __init__(self) -> None:
-        self._redis = Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     @call_history
     @count_calls
-    def store(self, data: Union[str, int, float, bytes]) -> str:
-        """Stores data in a redis cache"""
-        data_id = str(uuid4())
-        self._redis.set(data_id, data)
-        return data_id
+    def store(self, data:  Union[str, bytes, int, float]) -> str:
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
-    def get(self, key: str, **kwargs) -> (Union[str, int, float, bytes, None]):
-        """Retrieves data from the cache"""
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
         data = self._redis.get(key)
-        func = kwargs.get("fn", None)
-        return func(data) if func is not None else data
+        return fn(data) if fn is not None else data
 
-    def get_str(self, key: str):
-        """
-        Calls the internal get and passes
-        in a function to convert to str
-        """
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
 
-    def get_int(self, key: str):
-        """
-        Calls the internal get and
-        passes in a function to convert to int
-        """
-        return self.get(key, fn=lambda d: int(d))
+    def get_int(self, key: str) -> int:
+        '''Retrieves an integer value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: int(x))
+    
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,53 +1,38 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
+'''A module with tools for request caching and tracking.
+'''
 from redis import Redis
+from requests import get
 from functools import wraps
 from typing import Callable
 
-redis = Redis()
+
+redis_store = Redis()
+'''The module-level Redis instance.
+'''
 
 
-def check_cache(f: Callable) -> Callable:
-    """Checks the cache if the webpage is stored there"""
-
-    @wraps(f)
-    def wrapper(url: str) -> str:
-        """
-        wrapper preserves the functions documentation and
-        function name
-        """
-        redis.incr("count:{}".format(url))
-        web_page = redis.get(url)
-        if web_page:
-            return web_page.decode("utf-8")
-        web_page = f(url)
-        redis.set(url, f(url), 10)
-        redis.set("count:{}".format(url), 0)
-        return web_page
-
-    return wrapper
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
-@check_cache
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    In this tasks, we will implement a get_page function (prototype: def
-    get_page(url: str) -> str:). The core of the function is very simple.
-    It uses the requests module to obtain the HTML content of a particular
-    URL and returns it.
-
-    Start in a new file named web.py and do not reuse the code written in
-    exercise.py.
-
-    Inside get_page track how many times a particular URL was accessed in
-    the key "count:{url}" and cache the result with an expiration time of
-    10 seconds.
-
-    Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response
-    and test your caching.
-
-    Bonus: implement this use case with decorators.
-    """
-
-    from requests import get
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
     return get(url).text
